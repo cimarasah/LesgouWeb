8451ddceae80f6a52048cdac48bea67a63eec97f
“Cimara <cimarasah@gmail.com>
2022-02-24 16:48:14 -0300

    primeiro commit

---
+++ cimarasah.lesgou/cimarasah.lesgou/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cimarasah.lesgou.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
